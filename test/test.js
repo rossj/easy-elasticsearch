c9a5e000a92b430f19fdcb77fa58a99d9eaad554
Ross Johnson <ross@mazira.com>
2013-03-21 15:03:15 -0500

    Initial commit - partial API description

---
+++ test/test.js
@@ -0,0 +1,26 @@
+var
+// Node modules
+
+// Npm modules
+	_ = require('lodash'),
+
+// Project modules
+	elastic = require('./../lib/main.js');
+
+elastic.configure('localhost', 9200, 'testIndex', 'testType', 'testID');
+
+var doc = {
+	blahDoc : 'doc'
+};
+
+elastic.index(doc, function(err) {
+	console.log('Done!');
+	console.log(err);
+});
+
+elastic.index('aaa', doc, function(err) {
+	console.log('Done!');
+	console.log(err);
+});
+
+var elastic2 = new elastic.Client('localhost', 9200, 'test', 'type');
